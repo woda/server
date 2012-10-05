5f797f07cea1cdac43b5b798bf1629dbdd3231da
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2012-10-04 22:21:23 -0700

    Better unit tests

---
+++ spec/unit/helpers/log_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+require_corresponding __FILE__
+
+describe 'Colorize', :unit do
+  it 'should not crash when making string bold' do
+    'a'.bold
+    'a'.red.bold
+  end
+end
+
+describe Log, :unit do
+  it 'should not crash when logging' do
+    LOG.show_hello
+    LOG.fatal_hello { 'hello' }
+    LOG.hide_hello
+    LOG.fatal_hello { 'hello' }
+    lambda { LOG.hutehteuo }.should raise_error
+  end
+end
