de946f216db9a52bd45baeb0ac27a3deb2a11229
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2012-11-25 21:03:43 -0800

    new file and folder. Not used yet

---
+++ lib/models/folder.rb
@@ -0,0 +1,26 @@
+require 'data_mapper'
+
+class WFolder
+  include DataMapper::Resource
+  include WodaResource
+  
+  storage_names[:default] = "Folder"
+
+  property :id, Serial, key: true
+  updatable_property :name, String, index: true
+  updatable_property :last_modification_time, DateTime
+
+  has n, :access_rights
+  belongs_to :user, :child_key => :user_id
+  has n, :tree_relationships, :child_key => [ :parent ]
+  has n, :children, self, :through => :tree_relationships, :via => :child
+  has 1, :parent, self, :through => :tree_relationships, :via => :parent
+  has n, :x_files
+end
+
+class TreeRelationship
+  include DataMapper::Resource
+
+  belongs_to :child, 'WFolder', :key => true
+  belongs_to :parent, 'WFolder', :key => true
+end
---
+++ lib/models/xfile.rb
@@ -0,0 +1,17 @@
+require 'data_mapper'
+
+class XFile
+  include DataMapper::Resource
+  include WodaResource
+  
+  storage_names[:default] = "XFile"
+
+  property :id, Serial, key: true
+  updatable_property :name, String, index: true
+  updatable_property :last_modification_time, DateTime
+
+  has n, :access_rights
+  belongs_to :user, :child_key => :user_id
+  belongs_to :w_folder, :child_key => :parent_id
+#  has 1, :content
+end
