1a0d0223fe0c34f1dd3959538d707ea47614b3c8
guillo_k <kevin.guillouard@epitech.eu>
2013-02-20 01:20:17 -0500

    Begin tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe UsersController do
+  
+  before do
+    @controller = UsersController.new
+  end
+
+  it "shoud allow file listing" do
+    # TODO: Create test to do file listing
+  end
+
+  it "show allow a file listing from specific directory" do
+  
+  end
 
 end
