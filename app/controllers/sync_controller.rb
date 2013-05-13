0448bc8d6495910cbe6beac1f4beba5f2388db1c
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2013-05-13 04:49:10 -0700

    encrypting using s3

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -18,7 +18,7 @@ class SyncController < ApplicationController
  		set_content_files = [f]
  		# If it took more than 24 hours to upload the file, we just start over
  		if current_content && current_content.start_upload != 0 && current_content.start_upload < (Time.now.utc.to_i - 24 * 3600)
- 			set_content_files += WFile.find(content: current_content)
+ 			set_content_files += XFile.find(content: current_content).to_a
  			current_content = nil
  			delete_s3_file params['content_hash']
  		end
@@ -26,10 +26,10 @@ class SyncController < ApplicationController
  			@result = {success: true, need_upload: false, file: f}
  		else
  			current_content = Content.new(content_hash: params['content_hash'],
- 				size: params['size'],
+ 				size: params['size'].to_i,
  				crypt_key: WodaCrypt.new.random_key.to_hex,
  				init_vector: WodaCrypt.new.random_iv.to_hex,
- 				start_upload: Time.now.utc.to_i)
+ 				start_upload: Time.now.utc.to_i, file_type: 'none')
  			# TODO: not hardcode part size
 			@result = {success: true, need_upload: true, file: f, part_size: 5 * 1024 * 1024}
 		end
@@ -47,15 +47,13 @@ class SyncController < ApplicationController
 		raise RequestError.new(:bad_part, "Part number too high") if part > f.content.size / (5*1024*1024)
 		part_size = (part == f.content.size / (5*1024*1024) ? f.content.size % (5*1024*1024) : (5*1024*1024))
 		raise RequestError.new(:bad_part, "Size of part incorrect") unless part_size == params['data'].length
-		cypher = WodaCrypt.new
-		cypher.encrypt
-		cypher.iv = f.content.init_vector
-		cypher.key = f.content.crypt_key
 		s3 = AWS::S3.new
 		bucket = s3.buckets['woda-files']
 		obj = bucket.objects.create("#{f.content.content_hash}/#{params['part']}",
-			:data => cypher.update(params['data']) + cypher.final,
-			:content_type => 'octet-stream')
+			:data => params['data'],
+			:content_type => 'octet-stream',
+                        :server_side_encryption => :aes256,
+                        :encryption_key => f.content.crypt_key.from_hex)
 		@result = {success:true}
 	end
 
@@ -91,11 +89,7 @@ class SyncController < ApplicationController
 		f = session[:user].get_file(params['filename'].split('/'))
 		raise RequestError.new(:file_not_found, "File not found") unless f
 		s3 = AWS::S3.new
-		file = s3.buckets['woda-files'].objects["#{f.content.content_hash}/#{params['part']}"].read
-		cypher = WodaCrypt.new
-		cypher.decrypt
-		cypher.iv = f.content.init_vector
-		cypher.key = f.content.crypt_key
-		@result = cypher.update(file) + cypher.final
+		file = s3.buckets['woda-files'].objects["#{f.content.content_hash}/#{params['part']}"].read(:encryption_key => f.content.crypt_key.from_hex)
+		@result = file
 	end
 end
