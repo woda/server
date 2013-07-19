f68de078007830006ca9aa8eef47350077c15546
Adrien Ecoffet <adrien@debian>
2013-07-19 04:47:37 -0700

    better test

--- test.bash
+++ test.bash
@@ -13,8 +13,12 @@ else
 base=https://ec2-54-242-98-168.compute-1.amazonaws.com:3000
 fi
 
+login=
+while [ "$login" == '' ]
+do
 echo 'Enter login name:'
 read -r login
+done
 title 'Creating user:'
 echo_run curl -k -b cookies -c cookies -XPUT $base/users/$login -d "email=$login@gmail.co&password=hello&first_name=Adrien&last_name=Ecoffet"
 
@@ -36,14 +40,18 @@ echo_run curl -k -b cookies -c cookies -XGET $base/users
 title 'Changing self:'
 echo_run curl -k -b cookies -c cookies -XPOST $base/users -d "email=${login}.2@gmail.com"
 
+filedata=
+while [ "$filedata" == '' ]
+do
 title 'Enter file data:'
 read -r filedata
+done
 sha256=`echo -n "$filedata" | openssl dgst -sha256 | sed 's/(stdin)= //'`
 
 title 'Adding file:'
 echo_run curl -k -b cookies -c cookies -XPUT $base/sync/hello/world -d "content_hash=$sha256&size=3"
 
-title 'Sendig part:'
+title 'Sending part:'
 echo_run curl -k -b cookies -c cookies -XPUT $base/partsync/0/hello/world -d "$filedata"
 
 title 'Getting part:'
@@ -52,8 +60,39 @@ echo_run curl -k -b cookies -c cookies -XGET $base/partsync/0/hello/world
 title 'Uploading same file:'
 echo_run curl -k -b cookies -c cookies -XPUT $base/sync/file_2 -d "content_hash=$sha256&size=3"
 
+title 'Listing recent files:'
+echo_run curl -k -b cookies -c cookies -XGET $base/users/recents
+
+title 'Listing favorite files:'
+echo_run curl -k -b cookies -c cookies -XGET $base/users/favorites
+
+id=
+while [ "$id" == '' ]
+do
+title 'Setting favorite file (input ID):'
+read -r id
+done
+echo_run curl -k -b cookies -c cookies -XPOST $base/users/favorites/$id -d 'favorite=true'
+
+title 'Listing favorite files:'
+echo_run curl -k -b cookies -c cookies -XGET $base/users/favorites
+
+title 'Listing files:'
+echo_run curl -k -b cookies -c cookies -XGET $base/users/files
+
+title 'Making file public:'
+echo_run curl -k -b cookies -c cookies -XPOST $base/sync/public/hello/world -d 'status=true'
+
+title 'Synchronizing public file:'
+echo_run curl -k -b cookies -c cookies -XPUT $base/sync/foreign_public/wo -d "user=$login&foreign_filename=hello/world"
+
 title 'Listing files:'
 echo_run curl -k -b cookies -c cookies -XGET $base/users/files
 
 title 'Deleting file:'
 echo_run curl -k -b cookies -c cookies -XDELETE $base/sync/hello/world
+
+title 'Listing files:'
+echo_run curl -k -b cookies -c cookies -XGET $base/users/files
+
+echo
