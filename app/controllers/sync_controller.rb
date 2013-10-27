a62dd5a5e8121f0a546fa59ae146b90d65097bd5
kevindelordw <delord.kevin@gmail.com>
2013-10-28 00:27:45 +0100

    refactor sync_controller

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -3,43 +3,33 @@ require 'openssl'
 require 'digest/sha1'
 require 'tempfile'
 
-# BASE_URL='https://ec2-54-242-98-168.compute-1.amazonaws.com:3000'
-
 class SyncController < ApplicationController
   before_filter :require_login
-  # before_filter { |c| c.check_params :filename }
 
-  # before_filter Proc.new { |c| c.check_params :filename }, :only => [:upload_success, :delete]
+  before_filter Proc.new { |c| c.check_params :filename }, :only => [:upload_success, :delete]
   before_filter Proc.new { |c| c.check_params :content_hash, :size }, :only => [:put, :change]
   before_filter Proc.new { |c| c.check_params :part }, :only => [:upload_part, :get]
   before_filter Proc.new { |c| c.check_params :user, :foreign_filename }, :only => [:sync_public]
 
   def put
+    # TODO deal with: existing file not content
     current_content = Content.first content_hash: params['content_hash']
     f = session[:user].get_file(params['filename'].split('/'), create: true)
-    set_content_files = [f]
-#TODO: this needs to be uncommented
-    # If it took more than 24 hours to upload the file, we just start over
-#    if current_content && current_content.start_upload != 0 && current_content.start_upload < (Time.now.utc.to_i - 24 * 3600) && XFile.find(content: current_content)
-#      set_content_files += XFile.find(content: current_content).to_a
-#      current_content = nil
-#      delete_s3_file params['content_hash']
-#    end
     if current_content
       f.uploaded = true
       f.save
-      @result = {success: true, need_upload: false, file: f}
+      @result = {success: true, need_upload: false, file: f.description}
     else
       current_content = Content.new(content_hash: params['content_hash'],
                                     size: params['size'].to_i,
-                                    crypt_key: WodaCrypt.new.random_key.to_hex,
-                                    start_upload: Time.now.utc.to_i, file_type: 'none')      
-      @result = {success: true, need_upload: true, file: f, part_size: XFile.part_size }
+                                    crypt_key: WodaCrypt.new.random_key.to_hex)
+      @result = {success: true, need_upload: true, file: f.description, part_size: XFile.part_size }
       current_content.save
     end
-    set_content_files.each { |file| file.content = current_content }
+    f.content = current_content
+    f.last_update = Time.now
     session[:user].save
-    set_content_files.each { |file| file.save }
+    f.save
   end
 
   def upload_part
@@ -64,21 +54,18 @@ class SyncController < ApplicationController
     @result = { success: true }
   end
 
-  # TODO test
   def change
     file = session[:user].get_file(params['filename'].split('/'))
     raise RequestError.new(:read_only, "File is read-only") if file.read_only
     delete
     put
-    # @result = { success: true }
   end
 
-# TODO test
   def delete
     file = session[:user].get_file(params['filename'].split('/'))
     raise RequestError.new(:file_not_found, "File not found") unless file
     destroy_content = nil
-    if XFile.count(contents: file.contents) <= 1 then
+    if XFile.count(content_hash: file.content) <= 1 then
      destroy_content = file.content
     end
     file.destroy!
