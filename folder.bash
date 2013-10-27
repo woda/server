85e663cdab40a9fcf1ccafa3cea97b4e136cf991
kevindelordw <delord.kevin@gmail.com>
2013-10-28 00:26:44 +0100

    update bash scripts

--- folder.bash
+++ folder.bash
@@ -12,7 +12,6 @@ list() {
 }
 
 base=https://localhost:3000
-
 login=pltrflol
 
 title 'Logout:'
@@ -25,19 +24,12 @@ echo_run curl -k -b cookies -c cookies -XGET $base/users/logout
 title 'Logging in:'
 echo_run curl -k -b cookies -c cookies -XPOST $base/users/$login/login -d "password=hello"
 
-# filedata=
-# while [ "$filedata" == '' ]
-# do
-# title 'Enter file data:'
-# read -r filedata
-# done
-# filedata = "ertyehjnroferbfhjebfer"
-# sha256=`echo -n "$filedata" | openssl dgst -sha256 | sed 's/(stdin)= //'`
-
 list 
 
+foldername=folder1
+
 title 'Creating folder'
-echo_run curl -k -b cookies -c cookies -XPOST $base/folders/create -d 'path=folder1'
+echo_run curl -k -b cookies -c cookies -XPUT $base/folders/$foldername -d ""
 
 title 'set_favorite folder'
 echo_run curl -k -b cookies -c cookies -XPOST $base/folders/favorite -d 'path=folder1&favorite=true'
@@ -48,44 +40,8 @@ echo_run curl -k -b cookies -c cookies -XPOST $base/folders/public -d 'path=fold
 list 
 
 title 'delete folder'
-echo_run curl -k -b cookies -c cookies -XDELETE $base/folders/delete -d 'path=folder1'
+echo_run curl -k -b cookies -c cookies -XDELETE $base/folders/$foldername
 
 list
-
-  # match 'folders/create/:path' => 'folders#create', via: :post
-  # match 'folders/favorite/:path' => 'folders#favorite', via: :post
-  # match 'folders/public/:path' => 'folders#public', via: :post
-  # match 'folders/:path' => 'folders#delete', via: :delete
-
-
-# title 'Adding file:'
-# echo_run curl -k -b cookies -c cookies -XPUT $base/sync/helloworld -d "content_hash=$sha256&size=23"
-
-# title 'Sending part:'
-# echo_run curl -k -b cookies -c cookies -XPUT $base/partsync/0/hello/world -d "$filedata"
-
-# title 'Getting part:'
-# echo_run curl -k -b cookies -c cookies -XGET $base/partsync/0/hello/world
-
-# title 'Uploading same file:'
-# echo_run curl -k -b cookies -c cookies -XPUT $base/sync/file_2 -d "content_hash=$sha256&size=5"
-
-# id=
-# while [ "$id" == '' ]
-# do
-# title 'Setting favorite file (input ID):'
-# read -r id
-# done
-# echo_run curl -k -b cookies -c cookies -XPOST $base/files/favorite/$id -d 'favorite=true'
-
-
-# title 'Synchronizing public file:'
-# echo_run curl -k -b cookies -c cookies -XPUT $base/sync/foreign_public/wo -d "user=$login&foreign_filename=hello/world"
-
-# title 'Deleting file:'
-# echo_run curl -k -b cookies -c cookies -XDELETE $base/sync/hello/world
-
-# title 'Listing files:'
-# echo_run curl -k -b cookies -c cookies -XGET $base/users/files
 
 echo
