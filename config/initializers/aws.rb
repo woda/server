ff74b7f5cbc36a5a6a97e15596dec161df704d36
kevindelordw <delord.kevin@gmail.com>
2014-01-17 23:11:21 +0100

    fix default user space

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 BASE_URL='http://kobhqlt.fr:3000'
 PART_SIZE=5242880 # 5MB: 5 * 1024 * 1024
-DEFAULT_SPACE=1048576000 # 1GB: 1000 * 1024 * 1024
+DEFAULT_SPACE=1073741824 # 1GB: 1024 * 1024 * 1024
 
 Storage::path='./storage'
 Storage::use_aws = false
