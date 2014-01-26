509131fb6f0a4ae2c619fa98a234b2ccd2721c18
kevindelordw <delord.kevin@gmail.com>
2014-01-26 16:36:43 +0100

    update aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,8 +4,8 @@ DEFAULT_SPACE=1073741824 # 1GB: 1024 * 1024 * 1024
 
 Storage::path='./storage'
 Storage::use_aws = false
-AWS_ACCESS = 'AKIAIGXEIP24RN5TWCXQ'
-AWS_SECRET = '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'
+AWS_ACCESS = ''
+AWS_SECRET = ''
 
 AWS.config(:access_key_id => AWS_ACCESS, :secret_access_key => AWS_SECRET)
 
