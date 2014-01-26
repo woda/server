ea9f92fdcba22c089d2bd5a916292d3bae22acdd
kevindelordw <delord.kevin@gmail.com>
2014-01-26 16:37:27 +0100

    clean aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,11 @@
-BASE_URL='http://kobhqlt.fr:3000'
+BASE_URL=''
 PART_SIZE=5242880 # 5MB: 5 * 1024 * 1024
 DEFAULT_SPACE=1073741824 # 1GB: 1024 * 1024 * 1024
 
 Storage::path='./storage'
 Storage::use_aws = false
-AWS_ACCESS = 'AKIAIGXEIP24RN5TWCXQ'
-AWS_SECRET = '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'
+AWS_ACCESS = ''
+AWS_SECRET = ''
 
 AWS.config(:access_key_id => AWS_ACCESS, :secret_access_key => AWS_SECRET)
 
